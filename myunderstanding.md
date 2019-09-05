e03002a14053fdf90e5047c3b2fd6676ae9547a5
Jeff Pinckney <jeffpinckney.90@gmail.com>
2019-09-05 10:29:08 -0600

    working on displaying incomes and expenses

---
+++ myunderstanding.md
@@ -0,0 +1,2 @@
+from commit 2 to commit 3 i made changes because on commit 2 i was trying to update state with state. Thus state was successfully changing depending on inputs, but it was not actually saving anything. On commit 3 i added  the incomeCopy and expenseCopy variable in the income and expense method. while using the spread operator it is copying the input when the user hits the check mark and creates and object and stores it in the income array on state. object shorthand {descriptionInput, descriptionValue} is the name of the key:value pair in the new object being created
+
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,7 @@ export default class Main extends Component {
       descriptionValue: '',
       income: [],
       expense: [],
+      budget: 0,
       option: true,
 
     }
@@ -22,7 +23,6 @@ export default class Main extends Component {
     this.setState({
       descriptionInput: event.target.value
     })
-    console.log()
   }
 
   handleDescriptionValue(event) {
@@ -30,6 +30,10 @@ export default class Main extends Component {
       descriptionValue: event.target.value
     })
   }
+
+  /**
+   * Hits enter or Checkmark handlers
+   */
 
   income() {
     let { descriptionInput, descriptionValue } = this.state
@@ -39,7 +43,6 @@ export default class Main extends Component {
       descriptionInput: '',
       descriptionValue: ''
     })
-    console.log(this.data)
   }
 
   expense() {
@@ -50,8 +53,34 @@ export default class Main extends Component {
       descriptionInput: '',
       descriptionValue: ''
     })
-    console.log(this.data)
   }
+
+  displayIncomes() {
+    // ????
+    const { income } = this.state
+    const displayIncomes = income.map(income => {
+      return (
+        <div>
+          <li> { income.descriptionInput } </li>
+        </div>
+      )
+    })
+  }
+
+  displayExpenses() {
+    const { expense } = this.state
+    const displayExpenses = expense.map(expense => {
+      return (
+        <div>
+          <ul>
+            <li> { expense.descriptionInput } </li>
+          </ul>
+        </div>
+      )
+    })
+  }
+
+  /** */
 
   optionHandler() {
     this.setState({
@@ -62,9 +91,10 @@ export default class Main extends Component {
 
 
   render() {
+
     return (
       <div className='top'>
-        <img src={ Back } />
+        <img src={ Back } alt={ 'background' } />
         <div  >
           <div className='budget__title'> Available budget in <span className='budget__title--month'> %Month% </span> </div>
 
@@ -79,7 +109,7 @@ export default class Main extends Component {
             className='add__description'
             placeholder='Add Description'
             type='text'
-            value={this.state.descriptionInput}
+            value={ this.state.descriptionInput }
           ></input>
 
           <input
@@ -87,7 +117,7 @@ export default class Main extends Component {
             className='add__value'
             placeholder='Value'
             type='number'
-            value={this.state.descriptionValue}
+            value={ this.state.descriptionValue }
           ></input>
 
           <img
@@ -96,8 +126,13 @@ export default class Main extends Component {
             className='add__btn'
             onClick={ event => this.state.option ? this.income() : this.expense() }
           />
+
         </div>
+        { this.displayIncomes() }
+        { this.displayExpenses() }
       </div>
+
+
     )
   }
 }
